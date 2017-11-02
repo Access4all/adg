6eb9a60f8aaffc9ee558b9a772bb4622fd93731b
Thomas Jaggi <thomas.jaggi@unic.com>
2017-11-02 12:22:23 +0100

    Optimize code example

--- examples/dialog/demo.js
+++ examples/dialog/demo.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   let dialog = document.querySelector('[data-init="dialog"]')
 
-	dialog.classList.add('is-initialized')
+  dialog.classList.add('is-initialized')
 })
