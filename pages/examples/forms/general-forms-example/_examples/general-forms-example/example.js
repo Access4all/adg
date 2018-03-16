1d3b5415cbf4a1c5409161cb33a86b720c4cf8e8
Joshua Muheim <josh@josh.ch>
2018-03-16 11:59:58 +0100

    Prettify

--- pages/examples/forms/general-forms-example/_examples/general-forms-example/example.js
+++ pages/examples/forms/general-forms-example/_examples/general-forms-example/example.js
@@ -1,7 +1,4 @@
-(function() {
+;(function () {}.call(this))
 
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0FBQUEiLCJzb3VyY2VzQ29udGVudCI6WyIgICAgIl19
-//# sourceURL=coffeescript
+// # sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7O0FBQUEiLCJzb3VyY2VzQ29udGVudCI6WyIgICAgIl19
+// # sourceURL=coffeescript
