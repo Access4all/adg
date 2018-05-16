497e54435c8291c7b3b2dc4c7d37306ccbffb2db
Donato Rotunno <donato.rotunno@liip.ch>
2018-05-16 10:46:24 +0200

    Issue 51: allow inline javascript by overriding the 'validateLink' method

--- gulp/helpers/markdown.js
+++ gulp/helpers/markdown.js
@@ -20,6 +20,15 @@ module.exports = rootDir => filePath => {
   })
   const examples = requireNew('./examples')
 
+  markdown.validateLink = url => {
+    const BAD_PROTO_RE = /^(vbscript|file|data):/
+    const GOOD_DATA_RE = /^data:image\/(gif|png|jpeg|webp);/
+
+    const str = url.trim().toLowerCase()
+
+    return BAD_PROTO_RE.test(str) ? (GOOD_DATA_RE.test(str) ? true : false) : true
+  }
+
   return (
     markdown
       .use(plugins.abbr)
