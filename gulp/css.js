5515f2678b989cce0080a8e5f20a1858862f5898
Jürgen Rudigier <j.rudigier@googlemail.com>
2018-06-06 15:23:32 +0200

    feat: insert autoprefixer

--- gulp/css.js
+++ gulp/css.js
@@ -5,23 +5,25 @@ const nodeSassGlobbing = require('node-sass-globbing')
 const autoprefixer = require('autoprefixer')
 const postcss = require('gulp-postcss')
 
-
 module.exports = config => {
   return gulp
     .src(config.src, {
-  base: config.srcBase
+      base: config.srcBase
     })
     .pipe(
       sass({
-  importer: nodeSassGlobbing,
-  includePaths: config.includePaths
+        importer: nodeSassGlobbing,
+        includePaths: config.includePaths
       }).on('error', config.errorHandler)
     )
-  .pipe(postcss([
-      autoprefixer({ // browsers: see package.json > browserlist
-  flexbox: 'no-2009',
-  cascade: false
-      })
-    ]))
+    .pipe(
+      postcss([
+        autoprefixer({
+          // browsers: see package.json > browserlist
+          flexbox: 'no-2009',
+          cascade: false
+        })
+      ])
+    )
     .pipe(gulp.dest(config.dist))
 }
