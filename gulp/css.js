1d4d1da52d01534af5f977ff4bc9a82f3e4db738
Jürgen Rudigier <j.rudigier@googlemail.com>
2018-06-06 15:23:01 +0200

    feat: fix browser issues

--- gulp/css.js
+++ gulp/css.js
@@ -2,16 +2,26 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const nodeSassGlobbing = require('node-sass-globbing')
 
+const autoprefixer = require('autoprefixer')
+const postcss = require('gulp-postcss')
+
+
 module.exports = config => {
   return gulp
     .src(config.src, {
-      base: config.srcBase
+  base: config.srcBase
     })
     .pipe(
       sass({
-        importer: nodeSassGlobbing,
-        includePaths: config.includePaths
+  importer: nodeSassGlobbing,
+  includePaths: config.includePaths
       }).on('error', config.errorHandler)
     )
+  .pipe(postcss([
+      autoprefixer({ // browsers: see package.json > browserlist
+  flexbox: 'no-2009',
+  cascade: false
+      })
+    ]))
     .pipe(gulp.dest(config.dist))
 }
