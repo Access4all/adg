07d700fc71d50dd89f917acee0edef55983729c7
Donato Rotunno <donato.rotunno@liip.ch>
2018-06-06 10:39:14 +0200

    Issue 8: only use the original SS360 wdget, with search results as suggestions

--- src/assets/js/app/modules/Search.js
+++ src/assets/js/app/modules/Search.js
@@ -1,7 +1,6 @@
 import $ from 'jquery'
 
 import BaseModule from './BaseModule'
-import ADGAutocomplete from './adg/ADGAutocomplete'
 
 /**
  * Search
@@ -37,55 +36,10 @@ export default class Search extends BaseModule {
       specialMobileSuggest: {
         enabled: false
       },
-
-      // we assign this property here so we can use it in the "suggestChangeCallback" callback
-      searchBox: this.$el,
-
-      enterCallback: function () {
-        return false
-      },
-      suggestChangeCallback: function () {
-        var searchBox = window.ss360Config.searchBox,
-          suggests = searchBox.find('#unibox-suggest-box section'),
-          radiosList = $('<ul/>')
-
-        suggests.find('a').each(function (index, item) {
-          var id = 'search-suggests-' + index,
-            radio,
-            label,
-            item
-
-          radio = $('<input type="radio" name="search-suggests"/>')
-            .attr('value', $(this).attr('href'))
-            .attr('id', id)
-            .attr('class', 'visuallyhidden')
-
-          label = $('<label/>')
-            .attr('for', id)
-            .html($(this).html())
-
-          item = $('<li/>').append(radio, label)
-
-          radiosList.append(item)
-        })
-
-        suggests.replaceWith(radiosList)
-
-        // Update the options list
-        window.ADGAutocomplete.initOptions()
-        window.ADGAutocomplete.attachChangeEventToOptions()
-      }
+      suggestionsEqualSearch: true
     }
 
     initializeSs360()
-
-    this.$el
-      .find('input')
-      .on('adg-autocomplete-option-selected', function (e, option) {
-        window.location.assign(option.val())
-      })
-
-    window.ADGAutocomplete = new ADGAutocomplete(this.$el)
 
     this.$el.find('.search--toggle').one('click', function () {
       $(this)
