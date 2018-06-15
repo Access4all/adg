c7364d993ae2638e0b64bb2a3a471020aa25de92
Joshua Muheim <josh@josh.ch>
2018-06-15 12:21:54 +0200

    Prettify

--- pages/examples/sensible-aria-usage/pressed/_examples/marking-an-element-togglable-using-aria-pressed/example.js
+++ pages/examples/sensible-aria-usage/pressed/_examples/marking-an-element-togglable-using-aria-pressed/example.js
@@ -1,19 +1,18 @@
-(function() {
-  $(document).ready(function() {
-    var $button, audio;
-    $button = $("button");
-    audio = document.getElementById('audio');
-    audio.loop = true;
+;(function () {
+  $(document).ready(function () {
+    var $button, audio
+    $button = $('button')
+    audio = document.getElementById('audio')
+    audio.loop = true
     return $button.click(() => {
       if (audio.paused) {
-        audio.play();
-        return $button.attr("aria-pressed", true);
+        audio.play()
+        return $button.attr('aria-pressed', true)
       } else {
-        audio.pause();
-        audio.currentTime = 0;
-        return $button.attr("aria-pressed", false);
+        audio.pause()
+        audio.currentTime = 0
+        return $button.attr('aria-pressed', false)
       }
-    });
-  });
-
-}).call(this);
+    })
+  })
+}.call(this))
