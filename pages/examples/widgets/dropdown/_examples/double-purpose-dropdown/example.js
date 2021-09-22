3f5463ff52863ad7b4d0634905922072817213e8
Thomas Jaggi <thomas@responsive.ch>
2021-09-22 16:08:55 +0200

    feat: rewrite double-purpose dropdown example

--- pages/examples/widgets/dropdown/_examples/double-purpose-dropdown/example.js
+++ pages/examples/widgets/dropdown/_examples/double-purpose-dropdown/example.js
@@ -1,39 +1,44 @@
-;(function () {
-  var AdgDropdown
+class AdgDropdown {
+  constructor (element) {
+    this.element = element
+    this.buttons = this.element.querySelectorAll('[aria-expanded]')
 
-  AdgDropdown = class AdgDropdown {
-    constructor (el) {
-      this.$el = $(el)
-      this.initExpandables()
-    }
-
-    initExpandables () {
-      return this.$el.find('[aria-expanded]').click(e => {
-        var $button, $container
-        $button = $(e.target)
-        $container = $($button.next('*'))
-        if ($container.is(':visible')) {
-          return this.hide($button, $container)
-        } else {
-          return this.show($button, $container)
-        }
-      })
-    }
-
-    show ($button, $container) {
-      $container.attr('hidden', false)
-      return $button.attr('aria-expanded', true)
-    }
-
-    hide ($button, $container) {
-      $container.attr('hidden', true)
-      return $button.attr('aria-expanded', false)
-    }
+    this.init()
   }
 
-  $(document).ready(function () {
-    return $('[data-adg-dropdown]').each(function () {
-      return new AdgDropdown(this)
+  init () {
+    this.buttons.forEach(button => {
+      const containerId = button.getAttribute('aria-controls')
+      const container = document.querySelector(`#${containerId}`)
+
+      this.hide(button, container)
+
+      button.addEventListener('click', event => {
+        const button = event.target
+
+        if (!container.hidden) {
+          this.hide(button, container)
+        } else {
+          this.show(button, container)
+        }
+      })
     })
-  })
-}.call(this))
+  }
+
+  show (button, container) {
+    container.hidden = false
+    button.setAttribute('aria-expanded', true)
+  }
+
+  hide (button, container) {
+    container.hidden = true
+    button.setAttribute('aria-expanded', false)
+  }
+}
+
+// Init dropdowns and keep track of them
+const dropdowns = []
+
+document.querySelectorAll('[data-adg-dropdown]').forEach(element => {
+  dropdowns.push(new AdgDropdown(element))
+})
