08b071781a13c08bf8db8dbace3ced9d3da3dc1b
Christoph Dubach <christoph@dubach.dev>
2021-10-18 17:45:18 +0200

    ADG-142 Refactor radio buttons accordion

--- pages/examples/widgets/accordion/_examples/accordion-with-radio-buttons/example.js
+++ pages/examples/widgets/accordion/_examples/accordion-with-radio-buttons/example.js
@@ -1,21 +1,27 @@
-;(function () {
-  $(document).ready(function () {
-    return $("input[type='radio']").change(function () {
-      var $current_radiobutton, current_panel_id, current_radiobutton_name
-      $current_radiobutton = $(this)
-      current_radiobutton_name = $current_radiobutton.attr('name')
-      current_panel_id = `#${$current_radiobutton.attr('value')}_panel`
-      return $(`[name='${current_radiobutton_name}']`).each((i, element) => {
-        var $panel, $radiobutton, panel_id
-        $radiobutton = $(element)
-        panel_id = `#${$radiobutton.attr('id')}_panel`
-        $panel = $(panel_id)
-        if (panel_id === current_panel_id) {
-          return $panel.show()
-        } else {
-          return $panel.hide()
-        }
+class AdgRadioAccordion {
+  constructor (el) {
+    this.element = el
+    this.triggers = this.element.querySelectorAll('.trigger')
+    this.initTriggers()
+  }
+
+  initTriggers () {
+    this.triggers.forEach((trigger, triggerIndex, triggerArray) => {
+      const triggerRadio = trigger.querySelector('input[type="radio"]')
+      const panelId = `${triggerRadio.id}_panel`
+      const panel = document.getElementById(panelId)
+
+      triggerRadio.addEventListener('change', () => {
+        this.element.querySelectorAll('.panel').forEach(p => {
+          p.style.display = p.id === panel.id ? 'block' : 'none'
+        })
       })
     })
-  })
-}.call(this))
+  }
+}
+
+const accordions = []
+
+document
+  .querySelectorAll('[data-adg-radio-accordion]')
+  .forEach(element => accordions.push(new AdgRadioAccordion(element)))
