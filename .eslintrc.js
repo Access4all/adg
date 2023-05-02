edac86e825d899f51e4464a41a189e918ed3dcc8
Thomas Jaggi <thomas@responsive.ch>
2023-05-02 15:36:51 +0200

    chore: update eslint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-  extends: 'standard'
+  extends: ['standard', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 6
+  },
+  globals: {
+    $: true,
+    jQuery: true
+  },
+  env: {
+    node: true,
+    browser: true,
+    amd: true
+  },
+  // Disable rules the current code base does not follow
+  rules: {
+    'one-var': 'off',
+    'no-var': 'off',
+    'prefer-const': 'off',
+    camelcase: 'off',
+    'dot-notation': 'off',
+    'object-shorthand': 'off',
+    'no-useless-escape': 'off',
+    'no-unmodified-loop-condition': 'off',
+    'no-void': 'off'
+  }
 }
