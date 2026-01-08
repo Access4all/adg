974eafd57dd77a8807bc46c0f1688bcf43c4c74f
Christoph Dubach <christoph.dubach@unic.com>
2026-01-08 10:44:31 +0100

    ADG-464 feat: prevent runtime error

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -6,11 +6,12 @@ modules()
 // Init skip link
 document.addEventListener('DOMContentLoaded', () => {
   const skipLink = document.getElementById('jump')
-  if (!skipLink) return
-
   const skipTarget =
     document.getElementById('jump-target') ||
     document.getElementById('main-content')
+
+  if (!skipLink || !skipTarget) return
+
   skipLink.addEventListener('click', event => {
     skipTarget.setAttribute('tabindex', '-1')
     skipTarget.focus()
