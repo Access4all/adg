4ddb3c7a0b517d267bf487b9badd7118191b964e
Christoph Dubach <christoph.dubach@unic.com>
2026-01-08 10:26:51 +0100

    ADG-464 refactor: remove legacy jquery code, rename skip link target id

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -1,14 +1,18 @@
-import $ from 'jquery'
-
 import 'details-polyfill'
 import modules from './app/modules'
 
 modules()
 
-// Fixed broken skip link, see https://axesslab.com/skip-links/#update-1-a-better-solution
-$(document).ready(function () {
-  $('#jump').click(function (e) {
-    e.preventDefault()
-    $('#main-content').attr('tabindex', '-1').focus()
+// Init skip link
+document.addEventListener('DOMContentLoaded', () => {
+  const skipLink = document.getElementById('jump')
+  if (!skipLink) return
+
+  const skipTarget =
+    document.getElementById('jump-target') ||
+    document.getElementById('main-content')
+  skipLink.addEventListener('click', event => {
+    skipTarget.setAttribute('tabindex', '-1')
+    skipTarget.focus()
   })
 })
